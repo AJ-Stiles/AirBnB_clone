e170dcc6b521c73e2b58b0ff33843937c1e33491
Jude Alistair <jude55593@gmail.com>
2023-08-15 04:07:49 -0700

    all instances by class name

--- console.py
+++ console.py
@@ -5,7 +5,12 @@ Console module: Defines the HBNBCommand class for the command interpreter.
 
 import cmd
 from models.base_model import BaseModel
-import models
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
